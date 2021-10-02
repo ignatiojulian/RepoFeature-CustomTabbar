6d5fc39956d024cd5ac1622eeece2ee5d966744f
Ignatio <ignatio.tara@multipolar.com>
2021-10-02 16:15:03 +0800

    initial project

---
+++ custom-tabbar/custom-tabbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  custom-tabbar
+//
+//  Created by Ignatio Julian on 02/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
